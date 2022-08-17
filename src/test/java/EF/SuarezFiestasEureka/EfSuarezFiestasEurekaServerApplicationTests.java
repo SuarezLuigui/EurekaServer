5aa04df321bd2bce0baea27bc8893764cc5dea5d
SuarezLuigui <105759405+SuarezLuigui@users.noreply.github.com>
2022-08-16 22:57:22 -0500

    Eureka Server

---
+++ src/test/java/EF/SuarezFiestasEureka/EfSuarezFiestasEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package EF.SuarezFiestasEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfSuarezFiestasEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
